57759579538b6da379cf85c8cc0ce5324439fb0d
mrmaha <mrmaha2017@gmail.com>
2017-10-13 19:16:24 +0330

    add Forms

---
+++ Parts/Login.php
@@ -0,0 +1,7 @@
+<h1>Login</h1>
+
+<form action="" method="post" novalidate>
+	<div><input type="email" name="email" placeholder="email"></div>
+	<div><input type="password" name="password" placeholder="password"></div>
+	<input type="submit" value="Submit">
+</form>
